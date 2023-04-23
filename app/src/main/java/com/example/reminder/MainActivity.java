a6adbcd96d24c12cdd326fcf83662946b06feb51
Jorge <jorgebuendia@edix.com>
2023-04-23 23:26:18 +0200

    añadido calendario en activity main

--- app/src/main/java/com/example/reminder/MainActivity.java
+++ app/src/main/java/com/example/reminder/MainActivity.java
@@ -1,14 +1,57 @@
 package com.example.reminder;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
+    FirebaseAuth mAuth;
+    FirebaseFirestore db;
+    String emailUsuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        db = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+
+    //Creación del menú superior del activity main
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    //Funcionalidad de los botones del menú
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.Add:
+                //pasar a activity de add_notes
+                return true;
+
+            case R.id.Edit:
+                //pasar a activity de editar notas
+                return true;
+
+            case R.id.Logout:
+                mAuth.signOut();
+                onBackPressed();
+                finish();
+                return true;
+
+            default: return super.onOptionsItemSelected(item);
+
+        }
     }
 }
