f2248b60ddba6da6c8fcaf8dfa42e0dc7f223437
Isra <pirravk@gmail.com>
2023-04-23 17:14:26 +0200

    Creado el activity de añadir nota y parte de la funcionalidad

---
+++ app/src/main/java/com/example/reminder/ListNotes/ListNotes.java
@@ -0,0 +1,16 @@
+package com.example.reminder.ListNotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.reminder.R;
+
+public class ListNotes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_notes);
+    }
+}
