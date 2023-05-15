af97ea533ff8fc88821af604a771f21e3ec0f011
Isra <pirravk@gmail.com>
2023-05-16 01:11:30 +0200

    ListNotes: Action bar con la cabecera del activity y botón de atrás

--- app/src/main/java/com/example/reminder/ListNotes/ListNotes.java
+++ app/src/main/java/com/example/reminder/ListNotes/ListNotes.java
@@ -1,5 +1,6 @@
 package com.example.reminder.ListNotes;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -12,5 +13,17 @@ public class ListNotes extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_notes);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle("Event");
+            actionBar.setDisplayShowHomeEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return super.onSupportNavigateUp();
     }
 }
