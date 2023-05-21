8822905e7a5eb6454ee062b46e5a80df6cbc1fb3
Isra <pirravk@gmail.com>
2023-05-21 03:44:50 +0200

    Refactorización nombre de variables

--- app/src/main/java/com/example/reminder/MainActivity.java
+++ app/src/main/java/com/example/reminder/MainActivity.java
@@ -17,7 +17,7 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.reminder.UpdateNotes.UpdateEvents;
+import com.example.reminder.UpdateEvents.UpdateEvents;
 import com.example.reminder.ListEvents.ListEvents;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
